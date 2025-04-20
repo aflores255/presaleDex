70b960588e0bc1a87826b16c01db5cd197763f4c
Alberto <albertofm1992@gmail.com>
2025-04-20 20:05:35 +0300

    Added main functions

---
+++ src/PresaleDex.sol
@@ -0,0 +1,74 @@
+//1- License
+// SPDX-License-Identifier: MIT
+
+//2. Solidity Version
+pragma solidity 0.8.28;
+
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+// 3. Contract
+
+contract PresaleDex is Ownable {
+    using SafeERC20 for IERC20;
+    //Variables
+    address public usdtAddress;
+    address public usdcAddress;
+    address public fundsManager;
+    uint256 public maxSellAmount;
+    uint256[][3] public phases;
+    mapping(address => bool) public isBlacklisted;
+
+    //Constructor
+    constructor(
+        address usdtAddress_,
+        address usdcAddress_,
+        address fundsManager_,
+        uint256 maxSellAmount_,
+        uint256[][3] memory phases_
+    ) Ownable(msg.sender) {
+        usdcAddress = usdcAddress_;
+        usdtAddress = usdtAddress_;
+        fundsManager = fundsManager_;
+        maxSellAmount = maxSellAmount_;
+        phases = phases_;
+    }
+
+    //Functions
+
+    /**
+     * Add user to blacklist in order to avoid purchase of tokens
+     * @param user_ user to be added to the blacklist
+     */
+    function blackList(address user_) external onlyOwner {
+        isBlacklisted[user_] = true;
+    }
+
+    /**
+     * Remove user from the current blacklist
+     * @param user_ user to be removed from the blacklist
+     */
+    function removeFromBlackList(address user_) external onlyOwner {
+        isBlacklisted[user_] = false;
+    }
+
+    /**
+     * 
+     * 
+     */
+    function buyWithStable() external {
+        require(!isBlacklisted[msg.sender], "User blacklisted");
+    }
+
+    function emergencyWithdraw(address token_, uint256 amount_) onlyOwner() external{
+        IERC20(token_).safeTransfer(msg.sender,amount_);
+
+    }
+
+    function emergyWithdrawEther() onlyOwner() external{
+        uint256 scBalance = address(this).balance;
+        (bool success,) = msg.sender.call{value: scBalance}("");
+        require(success, "Withdraw failed");
+    }
+}
