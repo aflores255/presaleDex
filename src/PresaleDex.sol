a5c05f6f31cebde72a3a4f9724d9024b4af98d78
Alberto <albertofm1992@gmail.com>
2025-04-29 21:40:44 +0300

    added claim function

--- src/PresaleDex.sol
+++ src/PresaleDex.sol
@@ -15,6 +15,7 @@ contract PresaleDex is Ownable {
     using SafeERC20 for IERC20;
     //Variables
 
+    address presaleTokenAddress;
     address public usdtAddress;
     address public usdcAddress;
     address public fundsManager;
@@ -33,6 +34,7 @@ contract PresaleDex is Ownable {
 
     //Constructor
     constructor(
+        address presaleTokenAddress_,
         address usdtAddress_,
         address usdcAddress_,
         address dataFeedAddress_,
@@ -42,6 +44,7 @@ contract PresaleDex is Ownable {
         uint256 startTime_,
         uint256 endTime_
     ) Ownable(msg.sender) {
+        presaleTokenAddress = presaleTokenAddress_;
         usdcAddress = usdcAddress_;
         usdtAddress = usdtAddress_;
         fundsManager = fundsManager_;
@@ -53,6 +56,7 @@ contract PresaleDex is Ownable {
 
         require(endTime > startTime, "Incorrect time");
         require(startTime > block.timestamp, "Presale must be in the future");
+        IERC20(presaleTokenAddress).safeTransferFrom(msg.sender, address(this), maxSellAmount);
     }
 
     //Functions
@@ -103,6 +107,9 @@ contract PresaleDex is Ownable {
 
         emit TokenBuy(msg.sender, tokenAmountToReceive);
     }
+    /**
+     * Buy tokens with ether
+     */
 
     function buyWithEther() external payable {
         require(!isBlacklisted[msg.sender], "User blacklisted");
@@ -110,7 +117,7 @@ contract PresaleDex is Ownable {
 
         uint256 tokenAmountToReceive;
         uint256 usdValue = msg.value * getEtherPrice() / 1e18;
-        tokenAmountToReceive = usdValue * 1e6/ phases[currentPhase][1];
+        tokenAmountToReceive = usdValue * 1e6 / phases[currentPhase][1];
         checkCurrentPhase(tokenAmountToReceive);
         tokenSold += tokenAmountToReceive;
         require(tokenSold <= maxSellAmount, "Sold Out");
@@ -121,21 +128,48 @@ contract PresaleDex is Ownable {
         emit TokenBuy(msg.sender, tokenAmountToReceive);
     }
 
+    /**
+     * Get Ether current price using dataFeed
+     */
     function getEtherPrice() public view returns (uint256) {
         (, int256 price,,,) = IAggregator(dataFeedAddress).latestRoundData();
         price = price * (10 ** 10);
         return uint256(price);
     }
 
+    /**
+     * Withdraw tokens from SC only for owner
+     * @param token_ address of ERC token to withdraw
+     * @param amount_ amount of tokens to withdraw
+     */
     function emergencyWithdraw(address token_, uint256 amount_) external onlyOwner {
         IERC20(token_).safeTransfer(msg.sender, amount_);
     }
+    /**
+     * Withdraw ether from SC only for owner
+     */
 
     function emergyWithdrawEther() external onlyOwner {
         uint256 scBalance = address(this).balance;
         (bool success,) = msg.sender.call{value: scBalance}("");
         require(success, "Withdraw failed");
     }
+
+    /**
+     * Claim tokens once presale is finished
+     */
+    function claimTokens() external {
+        require(block.timestamp > endTime, "Presale not finished");
+
+        uint256 userAmount = userTokenBalance[msg.sender];
+        delete userTokenBalance[msg.sender];
+
+        IERC20(presaleTokenAddress).safeTransfer(msg.sender, userAmount);
+    }
+    /**
+     * Get current phase of the presale
+     * @param amount_ amount of tokens sold
+     */
 
     function checkCurrentPhase(uint256 amount_) private returns (uint256 phase) {
         if (
